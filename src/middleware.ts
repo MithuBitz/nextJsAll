7e47a87d10a347f34d9517700e78e18cf08d4267
Mithu Bortamuly <30743120+MithuBitz@users.noreply.github.com>
2024-04-26 17:31:31 +0530

    connect with mongo db and post a user useing postman

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,11 @@ import { NextResponse, NextRequest } from "next/server";
 export { default } from "next-auth/middleware";
 import { getToken } from "next-auth/jwt";
 
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/varify/:path*"],
+};
+
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
   //get the token from the request
@@ -20,10 +25,9 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  return NextResponse.redirect(new URL("/home", request.url));
-}
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/varify/:path*"],
-};
+  return NextResponse.next();
+}
